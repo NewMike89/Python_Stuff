4bd76cde9a3c901e638c50921e719eb3f66e0d08
Michael A Schorr <mschorr2014@gmail.com>
2019-02-16 22:02:54 -0500

    adding two more exercise programs

---
+++ remove_WS.py
@@ -0,0 +1,6 @@
+fav_lang = " Python "
+print("(" + fav_lang + ")")
+print("(" + fav_lang.rstrip() + ")")
+print("(" + fav_lang.lstrip() + ")")
+print("(" + fav_lang.strip() + ")")
+print("(" + fav_lang + ")")
