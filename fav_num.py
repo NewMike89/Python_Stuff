fd2d3d1ab33a6f91847785052d0ad953aaef7b69
Michael A Schorr <mschorr2014@gmail.com>
2019-02-19 19:53:49 -0500

    added comments to my fav_num program

--- fav_num.py
+++ fav_num.py
@@ -1,2 +1,6 @@
+# Michael Schorr
+# 2/19/19
+# Program takes my favorite number and adds it to a string.
+
 fav_num = 666
 print("My favorite number is " + str(fav_num) + "! May you be blessed by the damned.")
