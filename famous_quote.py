ad9d1317e16c9285a510002d8d60fb8c55e58577
Michael A Schorr <mschorr2014@gmail.com>
2019-02-20 17:37:44 -0500

    comments added to my famous_quote programs

--- famous_quote.py
+++ famous_quote.py
@@ -1,1 +1,5 @@
+# Michael Schorr
+# 2/19/19
+# program to display the use of single and double quotes with each other.
+
 print('"We can never obtain peace in the outer world until we make peace with ourselves." - 14th Dalai Lama, Tenzin Gyatso')
--- famous_quote2.py
+++ famous_quote2.py
@@ -1,2 +1,6 @@
+# Michael Schorr
+# 2/19/19
+# program to include variables with the use of the single and double quotes.
+
 famous_person = "Aristotle"
 print('"Pleasure in the job puts perfection in the work." - ' + famous_person)
