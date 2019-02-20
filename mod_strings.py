3db2db2f5c878c690bc12c962eff299fde48788c
Michael A Schorr <mschorr2014@gmail.com>
2019-02-19 20:09:22 -0500

    adding comments to the third set of programs

--- mod_strings.py
+++ mod_strings.py
@@ -1,3 +1,7 @@
+# Michael Schorr
+# 2/19/19
+# program takes vairiable NAME and sets different case modifiers to it.
+
 name = "karina luzada"
 print(name.title())
 print(name.upper())
--- mod_strings2.py
+++ mod_strings2.py
@@ -1,3 +1,7 @@
+# Michael Schorr
+# 2/19/19
+# same as the first MOD_STRINGS program but has more complex vairiable modifications.
+
 first_name = "karina"
 last_name = "luzada"
 full_name = first_name + " " + last_name
