ac923d9bbd09a11e53a13c43bf1bd890794ac77b
Michael A Schorr <mschorr2014@gmail.com>
2019-02-19 20:03:59 -0500

    adding comments to the next two programs

--- add_variables.py
+++ add_variables.py
@@ -1,5 +1,8 @@
+# Michael Schorr
+# 2/19/19
+# adding the usage of variables for the first time.
+
 msg1 = "Hello World,... again?"
 print(msg1)
-
 msg1 = "Changed the Variable value"
 print(msg1)
--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,5 @@
+# Michael Schorr
+# 2/19/19
+# First very basic program.
+
 print("hello world!")
