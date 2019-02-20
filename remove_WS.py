ae017ef36f4353bdcc30c0f046f8a5d916e5fa11
Michael A Schorr <mschorr2014@gmail.com>
2019-02-19 20:17:42 -0500

    adding comments to the to programs about STRING modifications so far

--- remove_WS.py
+++ remove_WS.py
@@ -1,3 +1,7 @@
+# Michael Schorr
+# 2/19/19
+# This program uses .(R/L)STRIP to remove WHITESPACE from strings.
+
 fav_lang = " Python "
 print("(" + fav_lang + ")")
 print("(" + fav_lang.rstrip() + ")")
--- tabs_newlines.py
+++ tabs_newlines.py
@@ -1,3 +1,7 @@
+# Michael Schorr
+# 2/19/19
+# Utilize the additions of NEW LINES and TABS.
+
 print("python")
 print("\tpython")
 print("Languages:\nPython\nC\nJavaScript")
