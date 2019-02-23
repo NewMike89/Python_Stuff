ae4e2c9b5965a8078821d3db3b35ee1a859d5fd6
Michael A Schorr <mschorr2014@gmail.com>
2019-02-22 21:20:32 -0500

    adding a lsit program that spells out my name

---
+++ list_3.py
@@ -0,0 +1,14 @@
+# Michael Schorr
+# 2/22/19
+# Listing the alphabet and searching through itself.
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+message = "The first leter of my name is " + letters[12].title() + "."
+print(letters[12])
+print(letters[8])
+print(letters[2])
+print(letters[7])
+print(letters[0])
+print(letters[4])
+print(letters[11])
+print(message)
