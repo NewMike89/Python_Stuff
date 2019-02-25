eff1eb68bf511974643d8db5b39728091fab574a
Michael A Schorr <mschorr2014@gmail.com>
2019-02-24 21:06:26 -0500

    excercise 3-2, greetings using the name list from before

---
+++ 3-2_greeting.py
@@ -0,0 +1,9 @@
+# Michael Schorr
+# 2/24/19
+# Using the original list of names and adding a greeting to each
+
+names = ["karina", "tamer", "mark", "justin"]
+print("Hello " + names[0].title() + ", how are you?")
+print("Whats up " + names[1].title() + ", keep on coding, dude!")
+print(names[2].title() + ", hows it been man?")
+print("Damn it " + names[3].title() + ", what did " + names[0].title() + " say!")
