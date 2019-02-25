2984ed0b613bccbe3cc27b00e2d1a4ad7802c34e
Michael A Schorr <mschorr2014@gmail.com>
2019-02-24 21:21:16 -0500

    exercise 3-3, a list of some vehicles in my life and statements about them

---
+++ 3-3_vehicle_list.py
@@ -0,0 +1,8 @@
+# Michael Schorr
+# 2/24/19
+# listing some vehicles and printing them in a few statements
+
+vehicles = ["toyota celica", "toyota tacoma", "chevy silverado"]
+print("I have always wanted to own a 1970s " + vehicles[0].title() + ", and I finally bought one in 2017.")
+print("My 2004 " + vehicles[1].title() + " was the most fun vehicle I have owned so far in my life.")
+print("A fully loaded 1984 blue " + vehicles[2].title() + " was my first truck when I was 16 years old.")
