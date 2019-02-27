06ebf22a09570c5af55434a8153c5555d4c0ea7f
Michael A Schorr <mschorr2014@gmail.com>
2019-02-26 20:33:35 -0500

    third modification program

---
+++ mod_list3.py
@@ -0,0 +1,7 @@
+# Michael Schorr
+# 2/26/19
+# Inserting list items at any index, depending on index specified.
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
