d315009eec0ae086bd734703faf497440067bb02
Michael A Schorr <mschorr2014@gmail.com>
2019-02-26 20:23:26 -0500

    second list modification program

---
+++ mod_list2.py
@@ -0,0 +1,19 @@
+# Michael Schorr
+# 2/26/19
+# Adding new items to the end of a list.
+
+# Part 1
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles.append('ducati')
+print(motorcycles)
+
+
+# Part 2
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+motorcycles.append('ducati')
+print(motorcycles)
