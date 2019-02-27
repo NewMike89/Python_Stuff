30b46ff62bcd211cd5bf77c89733cc79f3928ed7
Michael A Schorr <mschorr2014@gmail.com>
2019-02-26 20:16:09 -0500

    adding the first list modification program

---
+++ mod_list1.py
@@ -0,0 +1,9 @@
+# Michael Schorr
+# 2/26/19
+# Replacing list items with new onesself.
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
