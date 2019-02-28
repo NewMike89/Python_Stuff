5c101c75c23770c2d915e489839c7efb14413965
Michael A Schorr <mschorr2014@gmail.com>
2019-02-27 20:50:30 -0500

    exercise 3-5, removing declined guest and adding a new one

---
+++ 3-5changing_guest_list.py
@@ -0,0 +1,13 @@
+# Michael Schorr
+# 2/27/19
+# creating a list of anyone i want and inviting them to dinner.
+
+guests = ['george washington', 'bono', 'peter griffin']
+print("Greetings Mr. " + guests[0].title() + ", I would like to invite you to dinner on the 1st of March.")
+print(guests[1].title() + ". You, me, and two other blokes are goin out for some supper, March 1st. How bout it. YEAH YEAH YEAH!")
+print("Hey there " + guests[2].title() + ". You want some food? Then get your butt down to my place on the 1st of March.")
+print("WOMP WOMP, Bono declined!")
+guests[1] = 'chuck norris'
+print("Hey " + guests[1].title() + ", get on over to my place March 1st, and lets kick some ribs and chickens asses")
+print(guests[0].title() + ", Hope you are still going to make it on the first, next month.")
+print("Yo yo yo " + guests[2].title() + ", I know you will be there or be square....lol. you love food.")
