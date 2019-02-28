01b4f7c27a19f5a867c56c92ba3ac925f3e2035f
Michael A Schorr <mschorr2014@gmail.com>
2019-02-27 20:37:03 -0500

    exercise 3-4, inviting some people to dinner

---
+++ 3-4guest_list.py
@@ -0,0 +1,8 @@
+# Michael Schorr
+# 2/27/19
+# creating a list of anyone i want and inviting them to dinner.
+
+guests = ['george washington', 'bono', 'peter griffin']
+print("Greetings Mr. " + guests[0].title() + ", I would like to invite you to dinner on the 1st of March.")
+print(guests[1].title() + ". You, me, and two other blokes are goin out for some supper, March 1st. How bout it. YEAH YEAH YEAH!")
+print("Hey there " + guests[2].title() + ". You want some food? Then get your butt down to my place on the 1st of March.")
