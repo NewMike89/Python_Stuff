0f8f0ff723e69970e19dc3fe5e42a6f716da88a8
Michael A Schorr <mschorr2014@gmail.com>
2019-03-04 21:24:42 -0500

    program shows how to sort temporarily

---
+++ sort_lists2.py
@@ -0,0 +1,11 @@
+# Michael Schorr
+# 3/4/19
+# sorting a list but retaining the orignal order.
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here is the original list:")
+print(cars)
+print('\nHere is the sorted list:')
+print(sorted(cars))
+print("\nHere is the original list again:")
+print(cars)
