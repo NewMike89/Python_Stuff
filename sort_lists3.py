b6e6929a8b19f40de68410ddef4942aef131a152
Michael A Schorr <mschorr2014@gmail.com>
2019-03-04 21:34:09 -0500

    third list sorting program, with list length program

---
+++ sort_lists3.py
@@ -0,0 +1,13 @@
+# Michael Schorr
+# 3/4/19
+# How to reverse a list that is unsorted and finding list lengths.
+
+# REVERSE()
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.reverse()
+print(cars)
+
+# LEN()
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(len(cars))
