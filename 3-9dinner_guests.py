6f18a0b9856ce51c2fabd5f47e01686cf83ec480
Michael A Schorr <mschorr2014@gmail.com>
2019-03-13 19:18:55 -0400

    added comments to exercise 3-9

--- 3-9dinner_guests.py
+++ 3-9dinner_guests.py
@@ -1,3 +1,7 @@
+# Michael Schorr
+# 3/13/19
+# Adding a LEN() function to determine how many guests are in my list.
+
 guests = ['george washington', 'bono', 'peter griffin']
 print("Greetings Mr. " + guests[0].title() + ", I would like to invite you to dinner on the 1st of March.")
 print(guests[1].title() + ". You, me, and two other blokes are goin out for some supper, March 1st. How bout it. YEAH YEAH YEAH!")
