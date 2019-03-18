c97a43a9a8fecdbd84739b743c67b3381a6ba6ed
Michael A Schorr <mschorr2014@gmail.com>
2019-03-18 18:38:28 -0400

    exercise 4-1, FOR loop and displaying my favorite pizzas

---
+++ 4-1pizzas.py
@@ -0,0 +1,8 @@
+# Michael Schorr
+# 3/18/19
+# listing my favorite pizzas and using a loop to print some stuff about them.
+
+pizzas = ['buffalo chicken', 'zesty italian', 'pepperoni']
+for pizza in pizzas:
+    print(pizza.title() + ", is one of my favorite pizzas.")
+print("I really like pizza, so much so that I ate a whole large pizza the other day at work. Everyone was very suprized that I was able to do so but I have to say. I LOVE PIZZA!!")
