738cafedc27093710706d02aee479e70f652641e
Michael A Schorr <mschorr2014@gmail.com>
2019-03-21 18:34:08 -0400

    exercises 4-6 & 4-7, listing odds and multiples of 3, up to 20

---
+++ 4-6odds_&_4-7threes.py
@@ -0,0 +1,11 @@
+# Michael Schorr
+# 3/21/19
+# lists of odds and and 3s from 1 to 20.
+
+odds = list(range(1, 21, 2))
+for num in odds:
+    print(num)
+print("\n")
+threes = list(range(3, 21, 3))
+for nums in threes:
+    print(nums)
