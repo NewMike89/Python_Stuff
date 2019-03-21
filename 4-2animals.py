920c0365b9c46d01087849ef9b1bdd34655b26fb
Michael A Schorr <mschorr2014@gmail.com>
2019-03-20 20:09:45 -0400

    exercise 4-2, looping through a list of animals and printing information about them

---
+++ 4-2animals.py
@@ -0,0 +1,9 @@
+# Michael Schorr
+# 3/20/19
+# Using a FOR loop with some animals instead of pizzas.
+
+animals = ['tiger', 'lion', 'jaguar']
+for animal in animals:
+    print(animal.title())
+    print("A", animal.title() + ", would not make a great pet, they are far to dangerous, but one can dream\n")
+print("All of the following:\n\t" + animals[0].title(), "\n\t" + animals[1].title(), "\n\t" + animals[2].title(), "\nare big cats and very powerful.")
