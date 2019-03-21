aab458865154bdb514bdecdcb77584a2cc4dcc1f
Michael A Schorr <mschorr2014@gmail.com>
2019-03-20 20:21:19 -0400

    using the RANGE() function to create a list and use it with FOR loops

---
+++ numbers.py
@@ -0,0 +1,12 @@
+# Michael Schorr
+# 3/20/19
+# FOR loop with the RANGE() function and making a list with it.
+
+for value in range(1, 5):
+    print(value)
+print("\n")
+for values in range(1, 6):
+    print(values)
+print("\n")
+numbers = list(range(1, 6))
+print(numbers)
