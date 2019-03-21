f75293ed0401fa030b9af26a2707bbe5b7862308
Michael A Schorr <mschorr2014@gmail.com>
2019-03-21 18:14:48 -0400

    exercise 4-5, added 1 - 1 million together

---
+++ 4-5summing_1million.py
@@ -0,0 +1,8 @@
+# Michael Schorr
+# 3/21/19
+# Finding the MIN(), MAX(), and SUM() on 1 million numbers.
+
+million = list(range(1, 1000001))
+print(min(million))
+print(max(million))
+print(sum(million))
